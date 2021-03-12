70d1e3572997dbe98244d88c4a5286031cce317c
LucasKoval <lucaskoval@hotmail.com>
2021-03-11 21:14:40 -0300

    google.strategy added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Configuration } from './config/config.keys';
+import { ConfigModule } from './config/config.module';
+import { ConfigService } from './config/config.service';
+import { GoogleStrategy } from './google.strategy';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, GoogleStrategy],
 })
-export class AppModule {}
+export class AppModule {
+  static port: number | string;
+
+  constructor(private readonly _configService: ConfigService) {
+    AppModule.port = this._configService.get(Configuration.PORT);
+  }
+}
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,13 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World';
+  googleLogin(req) {
+    if (!req.user) {
+      return 'No user from Google'
+    }
+    return {
+      message: 'User info from Google',
+      user: req.user
+    }
   }
 }
